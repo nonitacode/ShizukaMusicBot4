415cdc0a56ddbcbc4c27812ab4bffe56cb16d1d9
NOBITA <nobitaanetwork@gmail.com>
2025-09-08 01:45:45 +0530

    Update help.py

--- BrandrdXMusic/utils/inline/help.py
+++ BrandrdXMusic/utils/inline/help.py
@@ -29,17 +29,14 @@ def help_pannel(_, START: Union[bool, int] = None):
                     )
                 ]  ,            [
                 InlineKeyboardButton(
-                    text=_["H_B_4"],
+                    text="👑𝐀𝐁𝐎𝐔𝐓 𝐌𝐄",
                     callback_data="help_callback hb4",
                 ),
                 InlineKeyboardButton(
-                    text=_["H_B_5"],
+                    text="😉𝐏𝐑𝐎𝐌𝐎𝐓𝐈𝐎𝐍",
                     callback_data="help_callback hb5",
                 ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
+                
             ]       ,       [
                     InlineKeyboardButton(
                         "🛠️ 𝗕𝗢𝗧 𝗠𝗔𝗦𝗧𝗘𝗥", url=f"https://t.me/KING_BOTS_MAKER"
